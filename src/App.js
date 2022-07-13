ee09c862ec1e767289d5b18209fff80f358edb91
igorm <igormatheussiquieri@gmail.com>
2022-07-12 22:10:46 -0300

    Criado contador com base da aula de state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    counter: 0
+  }
+
+  handleMoreClick = (e) => {
+    e.preventDefault()
+    const { counter } = this.state
+    this.setState({ counter: counter + 1 })
+  }
+
+  handleLessClick = (e) => {
+    e.preventDefault()
+    const { counter } = this.state
+    this.setState({ counter: counter - 1 })
+  }
+
+  render() {
+    const { counter } = this.state
+    return (
+      <div className="App">
+        <button onClick={this.handleLessClick}>-</button>
+        <div className="counter">{counter}</div>
+        <button onClick={this.handleMoreClick}>+</button>
+      </div >
+    )
+  }
 }
 
 export default App;
