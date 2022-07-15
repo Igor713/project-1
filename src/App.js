c115a740c32165e9fe5deb623fed8577fc7e122f
igorm <igormatheussiquieri@gmail.com>
2022-07-14 22:13:27 -0300

    Buscando dados externos com fetch

--- src/App.js
+++ src/App.js
@@ -3,31 +3,47 @@ import { Component } from 'react';
 
 class App extends Component {
   state = {
-    counter: 0
+    posts: []
+  };
+
+  componentDidMount() {
+    this.loadPosts();
   }
 
-  handleMoreClick = (e) => {
-    e.preventDefault()
-    const { counter } = this.state
-    this.setState({ counter: counter + 1 })
-  }
+  loadPosts = async () => {
+    const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts');
+    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos');
 
-  handleLessClick = (e) => {
-    e.preventDefault()
-    const { counter } = this.state
-    this.setState({ counter: counter - 1 })
+    const [posts, photos] = await Promise.all([postsResponse, photosResponse]);
+
+    const postsJson = await posts.json();
+    const photosJson = await photos.json();
+
+    const postsAndPhotos = postsJson.map((post, index) => {
+      return { ...post, cover: photosJson[index].url }
+    });
+
+    this.setState({ posts: postsAndPhotos });
   }
 
   render() {
-    const { counter } = this.state
+    const { posts } = this.state;
+
     return (
-      <div className="App">
-        <button onClick={this.handleLessClick}>-</button>
-        <div className="counter">{counter}</div>
-        <button onClick={this.handleMoreClick}>+</button>
-      </div >
-    )
+      <section className="container">
+        <div className="posts">
+          {posts.map(post => (
+            <div className="post">
+              <img src={post.cover} alt={post.title} />
+              <div key={post.id} className="post-content">
+                <h1>{post.title}</h1>
+                <p>{post.body}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+    );
   }
 }
-
 export default App;
