ff81ca6ec300f84d37f531960a9c9360e7301bc0
igorm <igormatheussiquieri@gmail.com>
2022-07-27 22:24:12 -0300

    Primeira aula de testes

---
+++ src/components/Button/Button.test.jsx
@@ -0,0 +1,42 @@
+import { render, screen, fireEvent, userEvent } from "@testing-library/react";
+import { Button } from '.';
+
+describe('<Button />', () => {
+    test('should render the button whith the text "Carregar mais posts"', () => {
+        render(<Button text="Carregar mais posts" />);
+        expect.assertions(1)
+
+        const button = screen.getByRole('button', { name: /carregar mais posts/i });
+        expect(button).toBeInTheDocument();
+    });
+
+    // Teste dois
+    test('should call function on button click', () => {
+        const fn = jest.fn();
+        render(<Button text="Carregar mais posts" onClick={fn} />);
+
+        const button = screen.getByRole('button', { name: /carregar mais posts/i });
+
+        fireEvent.click(button);
+
+        expect(fn).toHaveBeenCalledTimes(1);
+    });
+
+    // Teste três
+    test('should be disabled when disabled is true', () => {
+        render(<Button text="Carregar mais posts" disabled={true} />);
+
+        const button = screen.getByRole('button', { name: /carregar mais posts/i });
+
+        expect(button).toBeDisabled();
+    });
+
+    // Teste quatro
+    test('should be enabled when disabled is false', () => {
+        render(<Button text="Carregar mais posts" disabled={false} />);
+
+        const button = screen.getByRole('button', { name: /carregar mais posts/i });
+
+        expect(button).toBeEnabled();
+    });
+});
---
+++ src/templates/Home/Home.test.jsx
@@ -0,0 +1,13 @@
+describe('<Home />', () => {
+  test('one', () => {
+    expect(1).toBe(1);
+  });
+
+  test('two', () => {
+    expect(1).toBe(1);
+  });
+
+  test('three', () => {
+    expect(1).toBe(1);
+  });
+});
